ab8d631948c08590102daac8f88fa4738c157dcb
zhukovsergey <zhukov@etalon48.com>
2024-08-28 16:44:26 +0300

    16:44

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+import Header from "./components/Header";
+import Layout from "./components/layout";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>33</h1>
-    </>
+    <Layout>
+      
+        <Header />
+        
+      
+    </Layout>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between text-red-700">
+      <div>Logo</div>
+      <div>Djqnb</div>
+    </div>
+  );
+};
+
+export default Header;
